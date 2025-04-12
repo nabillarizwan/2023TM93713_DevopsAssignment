4c15f4b692a175432e65e572333d5a24e09ee478
Riyaz Ahmed <naahmed@ea.com>
2025-04-12 21:49:45 +0530

    updated tests

--- devops-pipeline/test/test_app.py
+++ devops-pipeline/test/test_app.py
@@ -3,8 +3,7 @@ from src.app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        #self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(2, 3), 10)  # wrong on purpose
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main()
